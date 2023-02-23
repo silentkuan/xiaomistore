40e103dd740b139ba2698b718300715e027e7135
silentkuan <101119449+silentkuan@users.noreply.github.com>
2023-02-23 16:13:46 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$db_host="us-cdbr-east-06.cleardb.net";
+$db_user="b5363ef00b2bfd";
+$db_pass="ffe3f4d0c66bdc3";
+$db_name="heroku_ffe3f4d0c66bdc3";
+
+
+$connect=mysqli_connect($db_host,$db_user,$db_pass,$db_name) or die("database connection error");
+?>
