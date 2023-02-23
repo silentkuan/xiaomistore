0a0ea4db69f5d116a1e74d1de607da1441bc0083
silentkuan <101119449+silentkuan@users.noreply.github.com>
2023-02-23 17:13:50 +0800

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,10 @@
 <?php
+$db_host="us-cdbr-east-06.cleardb.net";
+$db_user="b5363ef00b2bfd";
+$db_pass="5a71b740";
+$db_name="heroku_ffe3f4d0c66bdc3";
 
-$con = new mysqli("localhost","root","","xiaomi");
+$con = new mysqlimysqli_connect($db_host,$db_user,$db_pass,$db_name)；
 
 // Check connection
 if ($con -> connect_errno) {
