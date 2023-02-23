176b4665448013fa574b53d664a1b80e58a534c0
silentkuan <101119449+silentkuan@users.noreply.github.com>
2023-02-23 17:14:59 +0800

    Update db.php

--- admin/includes/db.php
+++ admin/includes/db.php
@@ -1,5 +1,9 @@
 <?php
+$db_host="us-cdbr-east-06.cleardb.net";
+$db_user="b5363ef00b2bfd";
+$db_pass="5a71b740";
+$db_name="heroku_ffe3f4d0c66bdc3";
 
 
-$con = mysqli_connect("localhost","root","","xiaomi");
+$con = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 ?>
